ed83a595eaba738b024c4cac8d80c0f489761ed7
Furkan Çakır <furkan.cakir@tgworkshop.com>
2021-12-05 21:55:25 +0300

    UI aictions and models created

--- SeturReport/Controllers/ReportController.cs
+++ SeturReport/Controllers/ReportController.cs
@@ -1,8 +1,11 @@
+using Confluent.Kafka;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using SeturReport.Models;
 using SeturReport.Models.Queries;
 using SeturReport.Models.Responses;
+using System.Net;
+using System.Threading;
 
 namespace SeturReport.Controllers
 {
@@ -41,6 +44,67 @@ namespace SeturReport.Controllers
 
                 return new ReportResponse<Report> { ErrorMessage = ex.Message, Type = ResponseType.Error };
             }
+        }
+
+        [HttpGet]
+        [Route("queue/request")]
+        public async Task< ReportResponse<Report>> ProcessReportQuery()
+        {
+            try
+            {
+                var config = new ProducerConfig
+                {
+                    BootstrapServers = "host1:9092,host2:9092",
+                    ClientId = Dns.GetHostName(),
+                };
+
+                using (var producer = new ProducerBuilder<Null, string>(config).Build())
+                {
+                    await producer.ProduceAsync("weblog", new Message<Null, string> { Value = "a log message" });
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return new ReportResponse<Report> { ErrorMessage = ex.Message, Type = ResponseType.Error };
+            }
+
+            return null;
+        }
+
+        [HttpGet]
+        [Route("queue/response")]
+        public async Task<ReportResponse<Report>> QueueReportResponse(ReportQuery query)
+        {
+            try
+            {
+                var config = new ConsumerConfig
+                {
+                    BootstrapServers = "host1:9092,host2:9092",
+                    GroupId = "foo",
+                    AutoOffsetReset = AutoOffsetReset.Earliest
+                };
+
+                using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+                {
+                    consumer.Subscribe("Report");
+
+                    //while (!cancelled)
+                    //{
+                    //    var consumeResult = consumer.Consume(cancellationToken);
+
+                    //}
+
+                    consumer.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return new ReportResponse<Report> { ErrorMessage = ex.Message, Type = ResponseType.Error };
+            }
+
+            return null;
         }
     }
 }
